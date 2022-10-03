c5a2685a93dadc93b2ba6ee07c03631669093413
Chase <chasehillig@yahoo.com>
2022-10-02 23:20:47 -0500

    add cors to application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [products, setProducts] = useState([{ name: "product1", price: 100.0 }])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+  }, [])
+
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
