df04d1e88165fa9ec31d5961ca86438baa92fc6c
Chase <chasehillig@yahoo.com>
2022-12-05 14:56:19 -0600

    minor changes

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,16 +1,53 @@
-import { AppBar, Toolbar, Typography, Switch } from "@mui/material"
+import { List } from "@mui/material"
+import { AppBar, Toolbar, Typography, Switch, ListItem } from "@mui/material"
+import { NavLink } from "react-router-dom"
 
 interface Props {
   darkMode: boolean
   handleThemeChange: () => void
 }
 
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+]
+
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+]
+
 export default function Header({ darkMode, handleThemeChange }: Props) {
   return (
-    <AppBar position='sticky' sx={{ mb: 4 }}>
+    <AppBar position='static' sx={{ mb: 4 }}>
       <Toolbar>
         <Typography variant='h6'>RE-STORE</Typography>
         <Switch checked={darkMode} onChange={handleThemeChange} />
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: "inherit", typography: "h6" }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <List sx={{ display: "flex" }}>
+          {rightLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: "inherit", typography: "h6" }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
       </Toolbar>
     </AppBar>
   )
